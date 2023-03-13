50ee58bef4bb5d55356f22df1e6661c08ab97cb4
Anatoly <75267969+zhukovanan@users.noreply.github.com>
2023-03-13 05:25:58 +0400

    Update stg_currencies.sql

--- src/sql/stg_currencies.sql
+++ src/sql/stg_currencies.sql
@@ -1,9 +1,23 @@
 CREATE TABLE IF NOT EXISTS ZHUKOVANANYANDEXRU__STAGING.currencies
-(currency_code int ,
-currency_code_with int,
-date_update DATE,
-currency_with_div NUMERIC (14,2),
-CONSTRAINT pk PRIMARY KEY (currency_code, currency_code_with, date_update) ENABLED)
+(
+  currency_code int,
+  currency_code_with int,
+  date_update DATE,
+  currency_with_div NUMERIC (14,2),
+  CONSTRAINT pk PRIMARY KEY (currency_code, currency_code_with, date_update) ENABLED
+)
 ORDER BY date_update
 SEGMENTED BY hash(currency_code) all nodes
-PARTITION BY date_update
+PARTITION BY date_update;
+
+CREATE PROJECTION IF NOT EXISTS ZHUKOVANANYANDEXRU__STAGING.currencies_proj AS
+SELECT
+    currency_code,
+    currency_code_with,
+    date_update
+    currency_with_div
+FROM
+    ZHUKOVANANYANDEXRU__STAGING.currencies
+ORDER BY
+    date_update
+SEGMENTED BY hash(currency_code) ALL NODES;
