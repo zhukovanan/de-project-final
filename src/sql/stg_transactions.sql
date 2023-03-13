c97bd9f65da2046e50282fcb7c2f7e2cc0a99678
Anatoly <75267969+zhukovanan@users.noreply.github.com>
2023-03-13 05:22:29 +0400

    Update stg_transactions.sql

--- src/sql/stg_transactions.sql
+++ src/sql/stg_transactions.sql
@@ -1,13 +1,32 @@
-CREATE TABLE IF NOT EXISTS ZHUKOVANANYANDEXRU__STAGING.transactions
-(operation_id uuid primary key,
-account_number_from int,
-account_number_to int,
-currency_code int,
-country varchar(20),
-status varchar(20),
-transaction_type varchar(30),
-amount int,
-transaction_dt TIMESTAMP)
-ORDER BY transaction_dt
+CREATE TABLE IF NOT EXISTS ZHUKOVANANYANDEXRU__STAGING.transactions 
+(
+  operation_id uuid primary key, 
+  account_number_from int, 
+  account_number_to int, 
+  currency_code int, 
+  country varchar(20), 
+  status varchar(20), 
+  transaction_type varchar(30), 
+  amount int, 
+  transaction_dt TIMESTAMP
+) 
+ORDER BY transaction_dt 
 SEGMENTED BY hash(operation_id) all nodes
-PARTITION BY transaction_dt::date
+PARTITION BY transaction_dt :: date;
+
+CREATE PROJECTION IF NOT EXISTS ZHUKOVANANYANDEXRU__STAGING.transactions_proj AS
+SELECT
+    operation_id,
+    account_number_from, 
+    account_number_to, 
+    currency_code, 
+    country, 
+    status, 
+    transaction_type, 
+    amount, 
+    transaction_dt
+FROM
+    ZHUKOVANANYANDEXRU__STAGING.transactions
+ORDER BY
+    transaction_dt
+SEGMENTED BY hash(operation_id) ALL NODES;
