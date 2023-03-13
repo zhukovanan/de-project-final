e17136155124c5d630543fa0d01de1fae6ba6c20
Anatoly <75267969+zhukovanan@users.noreply.github.com>
2023-03-13 05:28:42 +0400

    Update dwh_global_metrics.sql

--- src/sql/dwh_global_metrics.sql
+++ src/sql/dwh_global_metrics.sql
@@ -1,11 +1,27 @@
 CREATE TABLE IF NOT EXISTS ZHUKOVANANYANDEXRU__DWH.global_metrics
-(date_update date,
-currency_from int,
-amount_total Numeric(14,2),
-cnt_transactions int,
-avg_transactions_per_account Numeric(14,2),
-cnt_accounts_make_transactions int,
-CONSTRAINT pk PRIMARY KEY (date_update,currency_from))
+(
+  date_update date,
+  currency_from int,
+  amount_total Numeric(14,2),
+  cnt_transactions int,
+  avg_transactions_per_account Numeric(14,2),
+  cnt_accounts_make_transactions int,
+  CONSTRAINT pk PRIMARY KEY (date_update,currency_from)
+)
 ORDER BY date_update
 SEGMENTED BY hash(currency_from) all nodes
-PARTITION BY date_update
+PARTITION BY date_update;
+
+CREATE PROJECTION IF NOT EXISTS ZHUKOVANANYANDEXRU__DWH.global_metrics_proj AS
+SELECT
+    date_update,
+    currency_from,
+    amount_total,
+    cnt_transactions,
+    avg_transactions_per_account,
+    cnt_accounts_make_transactions
+FROM
+    ZHUKOVANANYANDEXRU__DWH.global_metrics
+ORDER BY
+    date_update
+SEGMENTED BY hash(currency_from) ALL NODES;
